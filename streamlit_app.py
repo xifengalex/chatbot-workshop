1256ac943e905b014e1d37ed6837924e8899be9e
xifengalex <xi.feng@sjsu.edu>
2025-02-12 14:26:41 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,15 @@ from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 from llama_index.embeddings.gemini import GeminiEmbedding
 
 
-st.set_page_config(page_title="Chat with a friend on the works of Rabindranath Tagore", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
-st.title("Chat with a friend on the works of Rabindranath Tagore")
+st.set_page_config(page_title="Ask the questions about Nutrition and Food Science Graduate Program", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.title("Ask the questions about Nutrition and Food Science Graduate Program")
 
 
 if "messages" not in st.session_state.keys():  # Initialize the chat messages history
     st.session_state.messages = [
         {
             "role": "assistant",
-            "content": "Ask me a question about Rabindranath Tagore!!",
+            "content": "Ask the questions about Nutrition and Food Science Graduate Program",
         }
     ]
 
