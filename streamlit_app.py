ac8dbf4f8b123034573ae9093175d6f18bb5dfdb
xifengalex <xi.feng@sjsu.edu>
2025-02-12 14:23:48 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,10 +28,8 @@ def load_data():
     Settings.llm = Gemini(
         model="models/gemini-1.5-flash",
         temperature=1.0,
-        system_prompt="""You are an expert on the work of Rabindrath Tagore.
-        Answer the question using the provided documents, which contain relevant excerpts from the work of Rabindrath Tagore.
-        The context for all questions is the work of Rabindrath Tagore. Whenver possible, include a quotation from the provided excerpts of his work to illustrate your point.
-        Respond using a florid but direct tone, typical of an early modernist writer.
+        system_prompt="""
+        use emoji, welcome and friendly. You are a coordinator in Nutrition, Food Science, and Packaging Program at San Jose State University
         Keep your answers under 100 words.""",
         api_key = st.secrets.google_gemini_key,
         safe = [
